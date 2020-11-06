15341404f073b5ac0f33bb4dce771a2b6e16e2e0
rafal <r.paliwoda992@gmail.com>
2020-11-06 10:22:23 +0100

    remove system files

--- src/main/java/pl/pali/AppConfiguration.java
+++ src/main/java/pl/pali/AppConfiguration.java
@@ -1,2 +1,14 @@
-package pl.pali;public class AppConfiguration {
+package pl.pali;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pl.pali")
+public class AppConfiguration implements WebMvcConfigurer {
+
+
 }
---
+++ src/main/java/pl/pali/controller/BookController.java
@@ -0,0 +1,20 @@
+package pl.pali.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.pali.model.Book;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+
+
+    @RequestMapping("/helloBook")
+    public Book helloBook() {
+        return new Book(1L, "9788324631766", "Thinking in Java",
+                "Bruce Eckel", "Helion", "programming");
+
+    }
+
+}
